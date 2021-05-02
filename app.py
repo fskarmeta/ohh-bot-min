901e273423ebd8bf4b0820c125f1dbe2e3ea9a18
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2021-05-02 04:45:47 -0400

    me tinca que ahora si

--- app.py
+++ app.py
@@ -236,6 +236,7 @@ async def play(ctx, url: str):
     song_there = os.path.isfile("song.mp3")
     try:
         if song_there:
+            print("Hay canción")
             os.remove("song.mp3")
     except PermissionError:
         await ctx.send("Espera que termine la canción o usa el commando #stop")
@@ -258,13 +259,13 @@ async def play(ctx, url: str):
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
-    for file in os.listdir("."):
+    for file in os.listdir("./"):
         if file.endswith(".mp3"):
             print("Encontro file .mp3")
-            os.rename(file, 'song.mp3')
+            os.rename(file, "song.mp3")
         else:
             print("no hay canción .mp3 en el directorio")
-        voice.play(discord.FFmpegPCMAudio("song.mp3"))
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
 
 
 @bot.command()
